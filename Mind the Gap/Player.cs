3317460fa2d846a60cc90b47fefd4f9fd3c87949
trebuHE <47220041+trebuHE@users.noreply.github.com>
2024-10-29 12:43:30 +0100

    add Player class

---
+++ Mind the Gap/Player.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mind_the_Gap
+{
+    internal class Player : Sprite
+    {
+        public Player(Texture2D texture, Vector2 position) : base(texture, position)
+        {
+
+        }
+    }
+}
