b445f771e04a4272cfbbec907a60753f6ccaec9c
trebuHE <47220041+trebuHE@users.noreply.github.com>
2024-11-03 17:36:32 +0100

    add IScene and clean-up

---
+++ Mind the Gap/IScene.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mind_the_Gap
+{
+    internal interface IScene
+    {
+        public void Load();
+        public void Update(GameTime gameTime);
+        public void Draw(SpriteBatch spriteBatch);
+    }
+}
